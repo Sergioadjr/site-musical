ac31399ad90a7dcec13c3fac4e13973a10d69738
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2022-05-18 14:57:23 -0400

    atualização

--- js/matricula.js
+++ js/matricula.js
@@ -1,27 +1,60 @@
-var totalCursos=document.querySelector(".js-total-de-cursos");
-var totalHoras=document.querySelector(".js-total-de-horas");
-var botaoConfirmar=document.querySelector(".js-botao-confirmar");
+var totalCursos = document.querySelector(".js-total-de-cursos");
+var totalHoras = document.querySelector(".js-total-de-horas");
+var totalValores = document.querySelector(".js-total-valores");
+var cursos = document.getElementsByName("curso")
 
-var quantidadeCursos=0;
-var quantidadeHoras=0;
+var botaoConfirmar = document.querySelector(".js-botao-confirmar");
 
-totalCursos.textContent=quantidadeCursos + "curso(s)"
+var quantidadeCursos = 0;
+var quantidadeHoras = 0;
+var quantidadeValores = 0;
+
+totalCursos.textContent = quantidadeCursos + "curso(s)"
 
 function adicionarCurso(checkbox) {
     if (checkbox.checked) {
         quantidadeCursos++;
         quantidadeHoras += parseInt(checkbox.value);
+        quantidadeValores += parseFloat(checkbox.getAttribute("custo"));
     }
     else {
         quantidadeCursos--;
         quantidadeHoras -= parseInt(checkbox.value);
+        quantidadeValores -= parseFloat(checkbox.getAttribute("custo"));
     }
-    if (quantidadeCursos>0){
-        botaoConfirmar.classList.remove("disabled")
-    }
-    else{
-        botaoConfirmar.classList.add("disabled")
-    }
+
+    habilitarBotao();
+    habilitarSelecionarTodos(checkbox);
     totalCursos.textContent = quantidadeCursos + "cursos(s)";
     totalHoras.textContent = quantidadeHoras + "h";
+    totalValores.textContent = quantidadeValores.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+
+
+}
+function habilitarBotao() {
+    if (quantidadeCursos > 0) {
+        botaoConfirmar.classList.remove("disabled")
+    }
+    else {
+        botaoConfirmar.classList.add("disabled")
+    }
+}
+
+function habilitarSelecionarTodos(checkbox) {
+    var selecionarTodos = document.querySelector("#js-cursos");
+    if (!selecionarTodos.checked && quantidadeCursos == cursos.length) {
+        selecionarTodos.checked = true;
+    }
+    else if (selecionarTodos.checked && !checkbox.checked) {
+        selecionarTodos.checked = false;
+    }
+}
+
+function selecionarTodos(checkbox) {
+    for (var indice = 0; indice < cursos.length; indice++) {
+        if (cursos[indice].checked != checkbox.checked) {
+            cursos[indice].checked = checkbox.checked;
+            adicionarCurso(cursos[indice]);
+        }
+    }
 }
